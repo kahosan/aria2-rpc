cc948627b9676d062a46da42f441b0a8b2ab078e
kahosan <15116355657l@gmail.com>
2023-05-08 13:19:10 +0800

    fix: export Client type

--- client.go
+++ client.go
@@ -9,20 +9,20 @@ import (
 	"github.com/kahosan/aria2-rpc/internal/resp"
 )
 
-type client struct {
+type Client struct {
 	call    func(method string, params, reply any) error
 	close   func() error
 	token   string
 	Options opts.Options
 }
 
-func NewClient(host string, token string) (*client, error) {
+func NewClient(host string, token string) (*Client, error) {
 	c, err := caller.NewCaller(host)
 	if err != nil {
 		return nil, err
 	}
 
-	return &client{
+	return &Client{
 		call:    c.Call,
 		close:   c.Close,
 		token:   token,
@@ -30,161 +30,161 @@ func NewClient(host string, token string) (*client, error) {
 	}, nil
 }
 
-func (c *client) Close() error {
+func (c *Client) Close() error {
 	return c.close()
 }
 
-func (c *client) AddURI(uris []string, options *opts.Options) (gid string, err error) {
+func (c *Client) AddURI(uris []string, options *opts.Options) (gid string, err error) {
 	c.call(method.AddURI, c.makeParams(uris, options), &gid)
 	return
 }
 
-func (c *client) AddTorrent(torrent *[]byte, uris *[]string, options *opts.Options) (gid string, err error) {
+func (c *Client) AddTorrent(torrent *[]byte, uris *[]string, options *opts.Options) (gid string, err error) {
 	et := base64.StdEncoding.EncodeToString(*torrent)
 	err = c.call(method.AddTorrent, c.makeParams(et, uris, options), &gid)
 	return
 }
 
-func (c *client) AddMetalink(metalink *[]byte, options *opts.Options) (gid []string, err error) {
+func (c *Client) AddMetalink(metalink *[]byte, options *opts.Options) (gid []string, err error) {
 	em := base64.StdEncoding.EncodeToString(*metalink)
 	err = c.call(method.AddMetalink, c.makeParams(em, options), &gid)
 	return
 }
 
-func (c *client) Remove(gid string) error {
+func (c *Client) Remove(gid string) error {
 	return c.call(method.Remove, c.makeParams(gid), nil)
 }
 
-func (c *client) ForceRemove(gid string) error {
+func (c *Client) ForceRemove(gid string) error {
 	return c.call(method.ForceRemove, c.makeParams(gid), nil)
 }
 
-func (c *client) Pause(gid string) error {
+func (c *Client) Pause(gid string) error {
 	return c.call(method.Pause, c.makeParams(gid), nil)
 }
 
-func (c *client) PauseAll() error {
+func (c *Client) PauseAll() error {
 	return c.call(method.PauseAll, c.makeParams(), nil)
 }
 
-func (c *client) ForcePause(gid string) error {
+func (c *Client) ForcePause(gid string) error {
 	return c.call(method.ForcePause, c.makeParams(), nil)
 }
 
-func (c *client) ForcePauseAll() error {
+func (c *Client) ForcePauseAll() error {
 	return c.call(method.ForcePauseAll, c.makeParams(), nil)
 }
 
-func (c *client) Unpause(gid string) error {
+func (c *Client) Unpause(gid string) error {
 	return c.call(method.Unpause, c.makeParams(gid), nil)
 }
 
-func (c *client) UnpauseAll() error {
+func (c *Client) UnpauseAll() error {
 	return c.call(method.UnpauseAll, c.makeParams(), nil)
 }
 
-func (c *client) TellStatus(gid string, keys ...string) (status resp.Status, err error) {
+func (c *Client) TellStatus(gid string, keys ...string) (status resp.Status, err error) {
 	err = c.call(method.TellStatus, c.makeParams(gid, keys), &status)
 	return
 }
 
-func (c *client) GetURIs(gid string) (uris []resp.URIs, err error) {
+func (c *Client) GetURIs(gid string) (uris []resp.URIs, err error) {
 	err = c.call(method.GetURIs, c.makeParams(gid), &uris)
 	return
 }
 
-func (c *client) GetFiles(gid string) (files []resp.Files, err error) {
+func (c *Client) GetFiles(gid string) (files []resp.Files, err error) {
 	err = c.call(method.GetFiles, c.makeParams(gid), &files)
 	return
 }
 
-func (c *client) GetPeers(gid string) (peers []resp.Peers, err error) {
+func (c *Client) GetPeers(gid string) (peers []resp.Peers, err error) {
 	err = c.call(method.GetPeers, c.makeParams(gid), &peers)
 	return
 }
 
-func (c *client) GetServers(gid string) (servers []resp.Servers, err error) {
+func (c *Client) GetServers(gid string) (servers []resp.Servers, err error) {
 	err = c.call(method.GetServers, c.makeParams(gid), &servers)
 	return
 }
 
-func (c *client) TellActive(keys ...string) (active []resp.Status, err error) {
+func (c *Client) TellActive(keys ...string) (active []resp.Status, err error) {
 	err = c.call(method.TellActive, c.makeParams(keys), &active)
 	return
 }
 
-func (c *client) TellWaiting(offset, num int, keys ...string) (waiting []resp.Status, err error) {
+func (c *Client) TellWaiting(offset, num int, keys ...string) (waiting []resp.Status, err error) {
 	err = c.call(method.TellWaiting, c.makeParams(offset, num, keys), &waiting)
 	return
 }
 
-func (c *client) TellStopped(offset, num int, keys ...string) (stopped []resp.Status, err error) {
+func (c *Client) TellStopped(offset, num int, keys ...string) (stopped []resp.Status, err error) {
 	err = c.call(method.TellStopped, c.makeParams(offset, num, keys), &stopped)
 	return
 }
 
-func (c *client) ChangePosition(gid string, pos int, how string) (err error) {
+func (c *Client) ChangePosition(gid string, pos int, how string) (err error) {
 	err = c.call(method.ChangePosition, c.makeParams(gid, pos, how), nil)
 	return
 }
 
-func (c *client) ChangeURI(gid string, fileIndex int, delURIs, addURIs *[]string, position int) (err error) {
+func (c *Client) ChangeURI(gid string, fileIndex int, delURIs, addURIs *[]string, position int) (err error) {
 	err = c.call(method.ChangeURI, c.makeParams(gid, fileIndex, delURIs, addURIs, position), nil)
 	return
 }
 
-func (c *client) GetOption(gid string) (options opts.Options, err error) {
+func (c *Client) GetOption(gid string) (options opts.Options, err error) {
 	err = c.call(method.GetOption, c.makeParams(gid), &options)
 	return
 }
 
-func (c *client) ChangeOption(gid string, options *opts.Options) (err error) {
+func (c *Client) ChangeOption(gid string, options *opts.Options) (err error) {
 	err = c.call(method.ChangeOption, c.makeParams(gid, options), nil)
 	return
 }
 
-func (c *client) GetGlobalOption() (options opts.Options, err error) {
+func (c *Client) GetGlobalOption() (options opts.Options, err error) {
 	err = c.call(method.GetGlobalOption, c.makeParams(), &options)
 	return
 }
 
-func (c *client) ChangeGlobalOption(options *opts.Options) (err error) {
+func (c *Client) ChangeGlobalOption(options *opts.Options) (err error) {
 	err = c.call(method.ChangeGlobalOption, c.makeParams(options), nil)
 	return
 }
 
-func (c *client) GetGlobalStat() (stat resp.GlobalStat, err error) {
+func (c *Client) GetGlobalStat() (stat resp.GlobalStat, err error) {
 	err = c.call(method.GetGlobalStat, c.makeParams(), &stat)
 	return
 }
 
-func (c *client) PurgeDownloadResult() error {
+func (c *Client) PurgeDownloadResult() error {
 	return c.call(method.PurgeDownloadResult, c.makeParams(), nil)
 }
 
-func (c *client) RemoveDownloadResult(gid string) error {
+func (c *Client) RemoveDownloadResult(gid string) error {
 	return c.call(method.RemoveDownloadResult, c.makeParams(gid), nil)
 }
 
-func (c *client) GetVersion() (version resp.Version, err error) {
+func (c *Client) GetVersion() (version resp.Version, err error) {
 	err = c.call(method.GetVersion, c.makeParams(), &version)
 	return
 }
 
-func (c *client) GetSessionInfo() (session resp.SessionInfo, err error) {
+func (c *Client) GetSessionInfo() (session resp.SessionInfo, err error) {
 	err = c.call(method.GetSessionInfo, c.makeParams(), &session)
 	return
 }
 
-func (c *client) Shutdown() error {
+func (c *Client) Shutdown() error {
 	return c.call(method.Shutdown, c.makeParams(), nil)
 }
 
-func (c *client) ForceShutdown() error {
+func (c *Client) ForceShutdown() error {
 	return c.call(method.ForceShutdown, c.makeParams(), nil)
 }
 
-func (c *client) SaveSession() error {
+func (c *Client) SaveSession() error {
 	return c.call(method.SaveSession, c.makeParams(), nil)
 }
 
@@ -194,17 +194,17 @@ type multiCallMethod struct {
 	Params []any  `json:"params"`     // Array containing parameters to the method call
 }
 
-func (c *client) MultiCall(methods *[]multiCallMethod) (result []any, err error) {
+func (c *Client) MultiCall(methods *[]multiCallMethod) (result []any, err error) {
 	c.call(method.Multicall, c.makeParams(methods), &result)
 	return
 }
 
-func (c *client) ListMethods() (methods []string, err error) {
+func (c *Client) ListMethods() (methods []string, err error) {
 	err = c.call(method.ListMethods, c.makeParams(), &methods)
 	return
 }
 
-func (c *client) makeParams(p ...any) []any {
+func (c *Client) makeParams(p ...any) []any {
 	params := make([]any, 0, 3)
 	if c.token != "" {
 		params = append(params, "token:"+c.token)
