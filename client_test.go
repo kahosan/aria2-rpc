4aca93f5f8680ab466e455a15a463bfe92914162
kahosan <15116355657l@gmail.com>
2023-05-16 13:44:53 +0800

    refactor: extract the notifier test

--- client_test.go
+++ client_test.go
@@ -1,27 +1,26 @@
-package ario
+package ario_test
 
 import (
 	"context"
 	"reflect"
-	"sync"
 	"testing"
 	"time"
 
+	ario "github.com/kahosan/aria2-rpc"
 	"github.com/kahosan/aria2-rpc/internal/testutils"
-	"github.com/kahosan/aria2-rpc/notifier"
 )
 
 func TestClient(t *testing.T) {
 
 	t.Run("unsupported scheme", func(t *testing.T) {
-		_, err := NewClient("localhost:6800/jsonrpc", "", false)
+		_, err := ario.NewClient("localhost:6800/jsonrpc", "", false)
 		if err.Error() != "unsupported scheme: localhost" {
 			t.Fatal("unexpected scheme check error")
 		}
 	})
 
 	// method test
-	client, err := NewClient(testutils.Arai2Uri("https://"), "", false)
+	client, err := ario.NewClient(testutils.Arai2Uri("https://"), "", false)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -43,7 +42,7 @@ func TestClient(t *testing.T) {
 	})
 
 	t.Run("add uri with options", func(t *testing.T) {
-		op := Options{}
+		op := ario.Options{}
 		op.Dir = "/tmp"
 
 		// the returned value does not include storage path information
@@ -69,7 +68,7 @@ func TestClient(t *testing.T) {
 	})
 
 	t.Run("gid status listener", func(t *testing.T) {
-		op := Options{}
+		op := ario.Options{}
 		op.Dir = "/tmp"
 		gid, err := client.AddURI([]string{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}, &op)
 		if err != nil {
@@ -178,140 +177,8 @@ func TestClient(t *testing.T) {
 	})
 }
 
-func TestNotifyListener(t *testing.T) {
-
-	t.Run("if notify is false, the listener will not be created", func(t *testing.T) {
-		_, err := NewClient(testutils.Arai2Uri("https://"), "", false)
-		if err != nil {
-			t.Fatal("should error")
-		}
-	})
-
-	client, err := NewClient(testutils.Arai2Uri("https://"), "", true)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer client.Close()
-
-	t.Run("if notify is true, the listener will be created", func(t *testing.T) {
-		notify, err := client.NotifyListener(context.Background())
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer notify.Close()
-
-		gid, err := client.AddURI([]string{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		var wg sync.WaitGroup
-		wg.Add(1)
-		// using coroutines to prevent blocking
-		go func() {
-			for v := range notify.Start() {
-				if v == gid {
-					t.Log("task start: ", v)
-				}
-			}
-		}()
-
-		go func() {
-			for v := range notify.Pause() {
-				if v == gid {
-					t.Log("task pause: ", v)
-				}
-			}
-		}()
-
-		go func() {
-			for v := range notify.Stop() {
-				if v == gid {
-					t.Log("task stop: ", v)
-					wg.Done()
-				}
-			}
-		}()
-
-		time.Sleep(time.Second)
-		err = client.Pause(gid)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		time.Sleep(time.Second)
-		err = client.Unpause(gid)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		time.Sleep(time.Second)
-		err = client.Remove(gid)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		wg.Wait()
-	})
-
-	t.Run("multiple Listen once tests", func(t *testing.T) {
-		notify, err := client.NotifyListener(context.Background())
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer notify.Close()
-
-		gid, err := client.AddURI([]string{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		var wg sync.WaitGroup
-		wg.Add(1)
-		go notify.ListenOnce(notifier.NotifyEvents.Start, func(g string, stop func()) {
-			if g == gid {
-				t.Log("start: ", gid)
-			}
-		})
-
-		go notify.ListenOnce(notifier.NotifyEvents.Pause, func(g string, stop func()) {
-			if g == gid {
-				t.Log("pause: ", gid)
-			}
-		})
-
-		go notify.ListenOnce(notifier.NotifyEvents.Stop, func(g string, stop func()) {
-			if g == gid {
-				t.Log("stop: ", gid)
-			}
-			wg.Done()
-		})
-
-		time.Sleep(time.Second)
-		err = client.Pause(gid)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		time.Sleep(time.Second)
-		err = client.Unpause(gid)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		time.Sleep(time.Second)
-		err = client.Remove(gid)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		wg.Wait()
-	})
-
-}
-
 func TestMultiCall(t *testing.T) {
-	client, err := NewClient(testutils.Arai2Uri("https://"), "", false)
+	client, err := ario.NewClient(testutils.Arai2Uri("https://"), "", false)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -325,16 +192,16 @@ func TestMultiCall(t *testing.T) {
 
 	t.Run("call multi add uri", func(t *testing.T) {
 		t.Run("multicall", func(t *testing.T) {
-			mc1 := MultiCallMethod{
+			mc1 := ario.MultiCallMethod{
 				Name:   "aria2.addUri",
 				Params: []any{[]any{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}},
 			}
-			mc2 := MultiCallMethod{
+			mc2 := ario.MultiCallMethod{
 				Name:   "aria2.addUri",
 				Params: []any{[]any{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}},
 			}
 
-			result, err := client.MultiCall(&[]MultiCallMethod{mc1, mc2})
+			result, err := client.MultiCall(&[]ario.MultiCallMethod{mc1, mc2})
 			if err != nil {
 				t.Fatal(err)
 			}
---
+++ notifier/notifier_test.go
@@ -0,0 +1,188 @@
+package notifier_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	ario "github.com/kahosan/aria2-rpc"
+	"github.com/kahosan/aria2-rpc/internal/testutils"
+	"github.com/kahosan/aria2-rpc/notifier"
+)
+
+func TestNotifyListener(t *testing.T) {
+
+	t.Run("if notify is false, the listener will not be created", func(t *testing.T) {
+		_, err := ario.NewClient(testutils.Arai2Uri("https://"), "", false)
+		if err != nil {
+			t.Fatal("should error")
+		}
+	})
+
+	client, err := ario.NewClient(testutils.Arai2Uri("https://"), "", true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	t.Run("if notify is true, the listener will be created", func(t *testing.T) {
+		notify, err := client.NotifyListener(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer notify.Close()
+
+		gid, err := client.AddURI([]string{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+		// using coroutines to prevent blocking
+		go func() {
+			for v := range notify.Start() {
+				if v == gid {
+					t.Log("task start: ", v)
+				}
+			}
+		}()
+
+		go func() {
+			for v := range notify.Pause() {
+				if v == gid {
+					t.Log("task pause: ", v)
+				}
+			}
+		}()
+
+		go func() {
+			for v := range notify.Stop() {
+				if v == gid {
+					t.Log("task stop: ", v)
+					wg.Done()
+				}
+			}
+		}()
+
+		time.Sleep(time.Second)
+		err = client.Pause(gid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		time.Sleep(time.Second)
+		err = client.Unpause(gid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		time.Sleep(time.Second)
+		err = client.Remove(gid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		wg.Wait()
+	})
+
+	t.Run("multiple Listen once tests", func(t *testing.T) {
+		notify, err := client.NotifyListener(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer notify.Close()
+
+		gid, err := client.AddURI([]string{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go notify.ListenOnce(notifier.NotifyEvents.Start, func(g string, stop func()) {
+			if g == gid {
+				t.Log("start: ", gid)
+			}
+		})
+
+		go notify.ListenOnce(notifier.NotifyEvents.Pause, func(g string, stop func()) {
+			if g == gid {
+				t.Log("pause: ", gid)
+			}
+		})
+
+		go notify.ListenOnce(notifier.NotifyEvents.Stop, func(g string, stop func()) {
+			if g == gid {
+				t.Log("stop: ", gid)
+			}
+			wg.Done()
+		})
+
+		time.Sleep(time.Second)
+		err = client.Pause(gid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		time.Sleep(time.Second)
+		err = client.Unpause(gid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		time.Sleep(time.Second)
+		err = client.Remove(gid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		wg.Wait()
+	})
+
+	t.Run("multiple Listen tests", func(t *testing.T) {
+		notify, err := client.NotifyListener(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer notify.Close()
+
+		gid, err := client.AddURI([]string{"https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso"}, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var wg = sync.WaitGroup{}
+		wg.Add(1)
+
+		task := notifier.Tasks{
+			notifier.NotifyEvents.Start: func(g string) {
+				if g == gid {
+					t.Log("下载开始: ", g)
+				}
+			},
+			notifier.NotifyEvents.Complete: func(g string) {
+				if g == gid {
+					t.Log("下载完成: ", g)
+					wg.Done()
+				}
+			},
+			notifier.NotifyEvents.Error: func(g string) {
+				if g == gid {
+					t.Log("下载失败: ", g)
+					wg.Done()
+				}
+			},
+			notifier.NotifyEvents.Stop: func(g string) {
+				if g == gid {
+					t.Log("下载移除: ", g)
+					wg.Done()
+				}
+			},
+		}
+
+		notify.ListenMultiple(task)
+		wg.Wait()
+	})
+}
